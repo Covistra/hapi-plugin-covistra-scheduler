a61c9550f4090b03743e71e6c3ba1307d181bf2e
Joel Grenon <joelgrenon@covistra.com>
2015-09-22 08:52:02 -0400

    Migrate to internal deps management

--- index.js
+++ index.js
@@ -21,58 +21,56 @@ var P = require('bluebird'),
     later = require('later'),
     _ = require('lodash');
 
+exports.deps = ['covistra-messaging'];
+
 exports.register = function (server, options, next) {
 
     var workers = {};
 
-    server.dependency(['covistra-messaging'], function(plugin, done) {
-        server.log(['plugin', 'info'], "Registering the scheduler plugin");
+    server.log(['plugin', 'info'], "Registering the scheduler plugin");
 
-        var Router = server.plugins['covistra-system'].Router;
+    var Router = server.plugins['covistra-system'].Router;
 
-        // Retrieve a reference to the current system configuration
-        var config = server.plugins['hapi-config'].CurrentConfiguration;
-        var log = server.plugins['covistra-system'].systemLog.child({plugin: 'scheduler'});
+    // Retrieve a reference to the current system configuration
+    var config = server.plugins['hapi-config'].CurrentConfiguration;
+    var log = server.plugins['covistra-system'].systemLog.child({plugin: 'scheduler'});
 
-        var scheduler = new Scheduler({host: config.get('REDIS_URL'), port: config.get("REDIS_PORT"), auth: config.get("REDIS_PASSWORD") });
+    var scheduler = new Scheduler({host: config.get('REDIS_URL'), port: config.get("REDIS_PORT"), auth: config.get("REDIS_PASSWORD") });
 
-        // Ensure that we're receiving Keyevent from Redis
-        scheduler.clients.scheduler.config("SET", "notify-keyspace-events", "Ex");
+    // Ensure that we're receiving Keyevent from Redis
+    scheduler.clients.scheduler.config("SET", "notify-keyspace-events", "Ex");
 
-        // Expose a few methods to manage jobs
-        plugin.expose('schedule', scheduler.schedule.bind(scheduler));
-        plugin.expose('scheduleWorker', function(workerKey, expiration, jobKey) {
-            jobKey = jobKey || _.uniqueId(workerKey);
-            log.debug("Scheduling worker %s at %s (%s)", workerKey, expiration, jobKey);
-            var shed;
+    // Expose a few methods to manage jobs
+    plugin.expose('schedule', scheduler.schedule.bind(scheduler));
+    plugin.expose('scheduleWorker', function(workerKey, expiration, jobKey) {
+        jobKey = jobKey || _.uniqueId(workerKey);
+        log.debug("Scheduling worker %s at %s (%s)", workerKey, expiration, jobKey);
+        var shed;
 
-            function recurHandler(err, jobKey) {
-                log.debug("Handling job", arguments);
-                scheduler.schedule({key: _.uniqueId(workerKey), expire: 50, handler: workers[workerKey]})
-            }
+        function recurHandler(err, jobKey) {
+            log.debug("Handling job", arguments);
+            scheduler.schedule({key: _.uniqueId(workerKey), expire: 50, handler: workers[workerKey]})
+        }
 
-            if(_.isString(expiration)) {
-                // Compute execution schedule using later.js
-                shed = later.parse.text(expiration);
-                return P.resolve(later.setInterval(recurHandler, shed));
-            }
-            else {
-                return P.promisify(scheduler.schedule, scheduler)({key: jobKey, expire: expiration, handler: workers[workerKey]});
-            }
-        });
-        plugin.expose('registerWorker', function(workerKey, handler) {
-            log.debug("Register a new worker %s", workerKey);
-            workers[workerKey] = handler;
-        });
-        plugin.expose('addHandler', scheduler.addHandler.bind(scheduler));
-        plugin.expose('reschedule', scheduler.reschedule.bind(scheduler));
-        plugin.expose('cancel', scheduler.cancel.bind(scheduler));
-
-        // Register routes
-        Router.routes(plugin, __dirname, "./routes");
-
-        done();
+        if(_.isString(expiration)) {
+            // Compute execution schedule using later.js
+            shed = later.parse.text(expiration);
+            return P.resolve(later.setInterval(recurHandler, shed));
+        }
+        else {
+            return P.promisify(scheduler.schedule, scheduler)({key: jobKey, expire: expiration, handler: workers[workerKey]});
+        }
     });
+    plugin.expose('registerWorker', function(workerKey, handler) {
+        log.debug("Register a new worker %s", workerKey);
+        workers[workerKey] = handler;
+    });
+    plugin.expose('addHandler', scheduler.addHandler.bind(scheduler));
+    plugin.expose('reschedule', scheduler.reschedule.bind(scheduler));
+    plugin.expose('cancel', scheduler.cancel.bind(scheduler));
+
+    // Register routes
+    Router.routes(plugin, __dirname, "./routes");
 
     next();
 };
